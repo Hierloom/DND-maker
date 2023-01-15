5ea5f17e61bb98f4665052d163f044090c5dfd8e
Niclas Hilmersson <40474518+Hierloom@users.noreply.github.com>
2023-01-15 14:25:35 +0100

    Updated navbar with naive ui classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
     extends: [
-      // add more generic rulesets here, such as:
-      "plugin:prettier/recommended",
-      'eslint:recommended',
-      'plugin:vue/vue3-recommended',
-      // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+        // add more generic rulesets here, such as:
+        "plugin:prettier/recommended",
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
     ],
     rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-      "sort-imports": ["error", {
+        // override/add rules settings here, such as:
+        // 'vue/no-unused-vars': 'error'
+        "sort-imports": [ "error", {
             "ignoreCase": false,
             "ignoreDeclarationSort": false,
             "ignoreMemberSort": false,
-            "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+            "memberSyntaxSortOrder": [ "none", "all", "multiple", "single" ],
             "allowSeparatedGroups": false
-        }]
+        } ]
     }
-  }
+};
--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,107 @@
 <script setup lang="ts">
-	import DescriptionCard from './components/DescriptionCard.vue';
+import { ref, h, Component, computed } from 'vue';
+import {
+    NSpace,
+    NButton,
+    NLayout,
+    NLayoutHeader,
+    NMenu,
+    NConfigProvider,
+} from 'naive-ui';
+import { RouterLink } from 'vue-router';
+import { darkTheme, NIcon } from 'naive-ui';
+import { DarkModeFilled, WbSunnyOutlined } from '@vicons/material';
+import type { MenuOption } from 'naive-ui';
+
+const inverted = ref(false);
+const lightThemeToggled = ref(false);
+
+const theme = computed<typeof darkTheme | null>(() => {
+    return lightThemeToggled.value ? null : darkTheme;
+});
+
+const menuOptions: MenuOption[] = [
+    {
+        label: () =>
+            h(
+                RouterLink,
+                { to: { name: 'create-character' } },
+                { default: () => 'Create character' }
+            ),
+        key: 'create-character',
+    },
+    {
+        label: () =>
+            h(
+                RouterLink,
+                { to: { name: 'interactive-character-creator' } },
+                { default: () => 'Classes' }
+            ),
+        key: 'interactive-character-creator',
+    },
+];
+
+function renderIcon(icon: Component) {
+    return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+function toggleTheme() {
+    lightThemeToggled.value = !lightThemeToggled.value;
+}
 </script>
 
 <template>
-	<div class="app-container">
-		<DescriptionCard>
-			<nav class="navbar">
-				<h1 class="">DND</h1>
-				<div class="links">
-					<router-link :to="{ name: 'create-character' }" class="link">Create character</router-link>
-					<router-link :to="{ name: 'interactive-character-creator' }" class="link">Classes</router-link>
-				</div>
-			</nav>
-		</DescriptionCard>
-		<main class="main">
-			<router-view></router-view>
-		</main>
-	</div>
+    <NConfigProvider :theme="theme">
+        <NSpace vertical>
+            <NLayout style="height: 100vh">
+                <NLayout-header>
+                    <nav class="navbar">
+                        <NButton tag="a" text size="small">DND</NButton>
+                        <NMenu mode="horizontal" :options="menuOptions" />
+                        <NButton size="tiny" circle @click="toggleTheme">
+                            <template #icon>
+                                <NIcon>
+                                    <WbSunnyOutlined v-if="lightThemeToggled" />
+                                    <DarkModeFilled v-else />
+                                </NIcon>
+                            </template>
+                        </NButton>
+                    </nav>
+                </NLayout-header>
+                <NLayout class="main">
+                    <router-view></router-view>
+                </NLayout>
+            </NLayout>
+        </NSpace>
+    </NConfigProvider>
 </template>
 
 <style lang="scss">
-	$navbarHeight: 50px;
-	.app-container {
-		height: 100%;
-	}
+$navbarHeight: 50px;
 
-	.navbar {
-		height: $navbarHeight;
-		max-height: $navbarHeight;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-	}
+.app-container {
+    height: 100%;
+}
 
-	.links {
-		display: flex;
+.navbar {
+    height: $navbarHeight;
+    max-height: $navbarHeight;
+    padding: var(--padding-2);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
-		& > .link {
-			margin-right: 1rem;
-		}
-	}
+.links {
+    display: flex;
 
-	.main {
-		height: calc(100% - $navbarHeight);
-		max-height: calc(100% - $navbarHeight);
-	}
+    & > .link {
+        margin-right: 1rem;
+    }
+}
+
+.main {
+    height: calc(100% - $navbarHeight);
+    max-height: calc(100% - $navbarHeight);
+}
 </style>
